0fd6db0094036e751de340037f751369e683925b
AnicetR <anicet.reglat@gmail.com>
2019-10-05 14:02:23 +0200

    Adds types and interfaces : gameobjects and renderLayer

---
+++ src/engine/types/gameObjectTypes.d.ts
@@ -0,0 +1,17 @@
+import { Sprite } from '../baseObjects/sprite';
+import { Context } from '../context';
+import { RenderLayer } from '../renderer/renderLayer';
+
+export type position = {
+    x: number,
+    y: number
+}
+
+export interface gameObjectInterface{
+    uuid: string;
+    position: position;
+    sprite: Sprite;
+    update(): void;
+    draw(context: Context): void;
+    registerToLayer(layer: RenderLayer): void;
+}
---
+++ src/engine/types/renderLayerInterface.d.ts
@@ -0,0 +1,11 @@
+import { Context } from "../context";
+import { gameObjectInterface } from "./gameObjectTypes";
+
+export interface RenderLayerInterface{
+    name: string;
+    gameObjects: Array<gameObjectInterface>;
+
+    addObject(gameObject: gameObjectInterface) : string
+    removeObject(uuid: string) : void
+    draw(context: Context) : void
+}
