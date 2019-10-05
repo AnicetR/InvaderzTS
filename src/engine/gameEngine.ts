57a098b127b7eb4d22e0d32d55c6792203e3ab16
AnicetR <anicet.reglat@gmail.com>
2019-10-05 14:06:10 +0200

    Let's use the renderer !

--- src/engine/gameEngine.ts
+++ src/engine/gameEngine.ts
@@ -1,9 +1,19 @@
 import { Context } from "./context";
+import { Renderer } from "./renderer/renderer";
 
+/**
+ * gameEngine manager
+ */
 export class gameEngine{
     public readonly context : Context;
+    public readonly renderer : Renderer;
 
+    /**
+     * Construct and launch the gameEngine
+     * @param canvas 
+     */
     constructor(private readonly canvas: HTMLCanvasElement){
         this.context = new Context(this.canvas);
+        this.renderer = new Renderer(this.context);
     }
 }
