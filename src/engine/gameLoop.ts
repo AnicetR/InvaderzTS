0105e78f2f4761c726fa10e1842ad8a117513424
AnicetR <anicet.reglat@gmail.com>
2019-10-25 17:08:33 +0200

    FPS tracking addition

--- src/engine/gameLoop.ts
+++ src/engine/gameLoop.ts
@@ -1,15 +1,20 @@
 import { Renderer } from "./renderer/renderer";
+import { fpsHistoryStore } from "./stores/debug/fpsHistoryStore";
 
 export class gameLoop{
 
     
 
-    //The game loop will be iterated 144 times per second
-    private _loopIterationsPerSecond: number = 40;
+    //The game loop will be iterated 30 times per second
+    private _loopIterationsPerSecond: number = 30;
     private _timeStep: number = 1000/this._loopIterationsPerSecond;
     
     private _isPaused: boolean = false;
     private _delta: number = 0;
+
+    //debug store and stuff
+    private _frameStore: fpsHistoryStore;
+
 
     /**
      * requestAnimationFrame window callback with polyfill
@@ -18,15 +23,16 @@ export class gameLoop{
         (window.requestAnimationFrame        ||
         window.webkitRequestAnimationFrame  ||
         function(callback: CallableFunction){
-            window.setTimeout( callback(Date.now()), 1000/this._maxFps );
+            window.setTimeout( callback(Date.now()), 1000/this._loopIterationsPerSecond );
         }).bind(window);
+    
 
     constructor(private readonly _renderer: Renderer){
-
+        this._frameStore = fpsHistoryStore.getInstance();
     }
 
     start() : void{
-        this.loop(0);
+        this.loop(performance.now());
     }
 
     private loop(currentTick: number) : void
@@ -58,6 +64,7 @@ export class gameLoop{
 
     private launchRender(currentTick: number): void
     {
+        this._frameStore.fpsLog(currentTick, this._renderer.lastTick);
         this._renderer.render(currentTick);
         this._requestAnimationFrame(this.loop.bind(this));
     }
---
+++ src/engine/stores/debug/fpsHistoryStore.ts
@@ -0,0 +1,89 @@
+import {createStore, createStoreObject, createEvent, Store} from 'effector'
+import { number } from 'prop-types';
+
+export class fpsHistoryStore{
+  private static instance: fpsHistoryStore;
+  private _store : any;
+  private _baseContext: Array<number> = [];
+  addFps: any;
+
+  private _fpsCache: number = 0;
+  private _fpsCacheStart: number = 0;
+  private _fpsCacheEnd: number = 0;
+
+  constructor(){
+    /**
+     * Defining store events
+     */
+    this.addFps = createEvent<Array<number>>('add fps');
+
+    /**
+     * Defining store and events logic
+     */
+    const store = createStore<Array<number>>(this._baseContext)
+                    .on(this.addFps, (state, fpsTimestamp: number): Array<number> => {
+                      if(state.length >= 30){
+                        state.shift();
+                      }
+                      return [...state, Math.round(fpsTimestamp)];
+                    });
+    /**
+     * Storing the store inside a property
+     */
+    this._store = createStoreObject<Store<Array<number>>>(store);
+  }
+  
+  /**
+   * Instanciate or returns the instance
+   */
+  static getInstance(): fpsHistoryStore {
+    if(!fpsHistoryStore.instance){
+      fpsHistoryStore.instance = new fpsHistoryStore();
+    }
+
+    return fpsHistoryStore.instance;
+  }
+
+  /**
+   * returns the state
+   */
+  get state(): any{
+    return this._store.getState()[0];
+  }
+
+  /**
+   * returns the store so he can be used by react
+   */
+  get store(): Store<Array<number>>{
+    return this._store;
+  }
+
+    fpsLog(currentTick : number, lastTick: number){
+    this._fpsCacheStart = performance.now();
+    if(this._fpsCacheStart <= this._fpsCacheEnd){
+      var currentFps = 1/((currentTick - lastTick)/1000);
+      if(this._fpsCache == 0){
+        this._fpsCache = currentFps;
+      }else{
+        this._fpsCache = (this._fpsCache + currentFps) / 2;
+      }
+    }else{
+        this._fpsCacheEnd = this._fpsCacheStart + 1000;
+        this.addFps(this._fpsCache);
+        this._fpsCache = 0;
+    }     
+}
+}
+
+
+/**
+   * Returns a FPS Histogram of the last 1800 fpss
+   */
+
+export const FpsHistogramList = fpsHistoryStore.getInstance().store.map(
+  (fpsHistory: Array<any>) => {
+      return fpsHistory[0].map((fps: number, key: number) => {
+        return {key: key, fps: fps};
+      });
+    }
+)
