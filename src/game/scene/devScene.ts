30ff87913708ae3e8875801b644e7db6a4092539
AnicetR <anicet.reglat@gmail.com>
2019-11-02 20:36:40 +0100

    random ennemy spawn

--- src/game/scene/devScene.ts
+++ src/game/scene/devScene.ts
@@ -4,6 +4,7 @@ import { RenderLayer } from "../../engine/renderer/renderLayer";
 import { background } from "./gameobjects/background";
 import { playerShip } from "./gameobjects/playerShip";
 import { ennemyShip } from "./gameobjects/enemyShip";
+import { position } from "../../engine/types/gameObjectTypes";
 
 export class devScene{
     constructor(private readonly gameEngineInstance: gameEngine){
@@ -16,6 +17,32 @@ export class devScene{
         new background(backgroundLayer, this.gameEngineInstance);
 
         new playerShip().registerToLayer(playerLayer);
-        new ennemyShip().registerToLayer(playerLayer);
+
+        setInterval(() => {
+            let lastPosition: position = {x: 0, y: 0};
+            const setPosition = (): position => ({
+                x: Math.random() * this.gameEngineInstance.context.boundaries.maxX,
+                y: -100
+            })
+
+            for(let i = 0; i < (Math.random()*3); i++){
+                let position : position = setPosition(); 
+                let velocity : number = Math.min(0.015, Math.random() * 0.05);
+                if(Math.random() > 0.5){
+                    velocity = velocity * -1;
+                }
+                while(
+                    (position.x < lastPosition.x - 100) 
+                    && (position.x > lastPosition.x + 100)
+                    ) {
+                        position = setPosition();
+                    }
+                new ennemyShip(position, velocity).registerToLayer(playerLayer);
+                lastPosition = position;
+            }
+        }, 4000)
+        
     }
+
+    
 }
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { spaceinvaderz } from "./game/spaceinvaderz";
 
 //createContextCanvas
 const canvas: HTMLCanvasElement = document.createElement('canvas');
-canvas.height = 800;
+canvas.height = 900;
 canvas.width = 750;
 
 document.body.append(canvas);
 
-new spaceinvaderz(canvas);
+new spaceinvaderz(canvas); 
