8eff9afc8f5e17306c6951e574520914c4e9a73f
AnicetR <anicet.reglat@gmail.com>
2019-11-04 09:49:07 +0100

    rewrite game bootstrap : loadGame as a promise

--- src/game/spaceinvaderz.tsx
+++ src/game/spaceinvaderz.tsx
@@ -2,36 +2,43 @@ import { gameEngine } from "../engine/gameEngine";
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
 import UI from './ui/ui';
 import { devScene } from "./scene/devScene";
-import { sceneManager } from "../engine/sceneManager";
 import { testScene } from "./scene/testScene";
-
 export class spaceinvaderz{
-    private readonly gameEngine: gameEngine;
 
     constructor(private readonly canvas: HTMLCanvasElement){
-        this.gameEngine = gameEngine.getInstance(canvas);
-        const sceneMgr = new sceneManager();
-        sceneMgr.scenesCollection.set('dev', new devScene(this.gameEngine));
-        sceneMgr.scenesCollection.set('test', new testScene(this.gameEngine));
-        
-        sceneMgr.changeScene('dev');
-        
-        setTimeout(() => sceneMgr.changeScene('test'), 5000);
+        this.loadGame(this.canvas).then(() => this.loadUi());
+    }
 
-        const uiContainer: HTMLElement = document.getElementById('ui');
+    
 
-        ReactDOM.render(
-          <UI></UI>,
-           uiContainer
+    loadGame(canvas: HTMLCanvasElement) : Promise<gameEngine>{
+        return new Promise((resolve: PromiseResolve<gameEngine>, reject: PromiseReject): void => { 
+            const engine : gameEngine = gameEngine.getInstance(canvas);
+            engine.sceneMgr.scenesCollection.set('dev', new devScene(engine));
+            engine.sceneMgr.scenesCollection.set('test', new testScene(engine));
+            resolve(engine);
+        }); 
+    }
+
+    loadUi(){
+            const uiContainer: HTMLElement = document.getElementById('ui');
+
+            ReactDOM.render(
+            <UI></UI>
+            , uiContainer
         );
-        
+                        
         uiContainer.style.height = ""+this.canvas.height+"px";
         uiContainer.style.width = ""+this.canvas.width+"px";
         uiContainer.style.position = "absolute";
     }
 }
+
+type PromiseResolve<T> = (value?: T | PromiseLike<T>) => void; 
+type PromiseReject = (error?: any) => void;
 
 
 
